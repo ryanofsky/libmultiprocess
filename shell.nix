151ef65a7c91147aa14bfc1b044e997d973b44e8
Ryan Ofsky <ryan@ofsky.org>
2025-08-12 16:00:38 -0400

    ci: Add olddeps job to old versions of dependencies
    The CI job currently just tests old Cap'n Proto versions, but it might be nice
    to extend in the future to test old compilers & build tools too.
    
    Support for versions of Cap'n Proto before 0.7.0 was dropped in
    https://github.com/bitcoin-core/libmultiprocess/pull/88 in order to avoid
    compiler warnings and simplify code. Before that, versions back to 0.5 were
    supported and are basically still compatible since the Cap'n Proto API hasn't
    changed and libmultiprocess does not rely on newer features.

--- shell.nix
+++ shell.nix
@@ -2,12 +2,28 @@
 , crossPkgs ? import <nixpkgs> {}
 , enableLibcxx ? false # Whether to use libc++ toolchain and libraries instead of libstdc++
 , minimal ? false # Whether to create minimal shell without extra tools (faster when cross compiling)
+, capnprotoVersion ? null
 }:
 
 let
   lib  = pkgs.lib;
   llvm = crossPkgs.llvmPackages_20;
-  capnproto = crossPkgs.capnproto.override (lib.optionalAttrs enableLibcxx { clangStdenv = llvm.libcxxStdenv; });
+  capnprotoHashes = {
+    "0.7.0" = "sha256-Y/7dUOQPDHjniuKNRw3j8dG1NI9f/aRWpf8V0WzV9k8=";
+    "0.8.0" = "sha256-rfiqN83begjJ9eYjtr21/tk1GJBjmeVfa3C3dZBJ93w=";
+  };
+  capnprotoBase = if capnprotoVersion == null then crossPkgs.capnproto else crossPkgs.capnproto.overrideAttrs (old: {
+    version = capnprotoVersion;
+    src = crossPkgs.fetchFromGitHub {
+      owner = "capnproto";
+      repo  = "capnproto";
+      rev   = "v${capnprotoVersion}";
+      hash  = lib.attrByPath [capnprotoVersion] "" capnprotoHashes;
+    };
+    env = {};
+    patches = [];
+  });
+  capnproto = capnprotoBase.override (lib.optionalAttrs enableLibcxx { clangStdenv = llvm.libcxxStdenv; });
   clang = if enableLibcxx then llvm.libcxxClang else llvm.clang;
   clang-tools = llvm.clang-tools.override { inherit enableLibcxx; };
 in crossPkgs.mkShell {
