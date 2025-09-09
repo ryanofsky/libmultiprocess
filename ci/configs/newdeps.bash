2c86c5aa0b179c5009fba2f86af1efb535882f83
Ryan Ofsky <ryan@ofsky.org>
2025-09-09 14:20:44 -0400

    cmake: Increase cmake policy version
    Increase cmake policy version from 3.12 to 4.1 in standalone builds to stop
    using very old and deprecated CMake policies in standalone builds.
    
    Also stop overriding policy version if a parent project has already set one, so
    parent projects are able to control which policies are used by default based on
    their own practices and needs.
    
    In the Bitcoin Core subtree, this change causes the libmultiprocess policy
    version to increase from 3.12 to 3.22, which is the version Bitcoin Core sets.
    
    This commit only changes the policy version, it does not change the specified
    minimum version of cmake required to build the project, which is 3.12.

--- ci/configs/newdeps.bash
+++ ci/configs/newdeps.bash
@@ -2,5 +2,6 @@ CI_DESC="CI job using newest Cap'n Proto and cmake versions"
 CI_DIR=build-newdeps
 export CXXFLAGS="-Werror -Wall -Wextra -Wpedantic -Wno-unused-parameter -Wno-error=array-bounds"
 CAPNP_CHECKOUT=master
+# cmakeVersion here should match policy version in CMakeLists.txt
 NIX_ARGS=(--argstr capnprotoVersion "none" --argstr cmakeVersion "4.1.1")
 BUILD_ARGS=(-k)
