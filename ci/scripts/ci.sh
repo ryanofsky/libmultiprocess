eac91e08f477f7e70601707530d8db822dbc74e3
Ryan Ofsky <ryan@ofsky.org>
2025-09-10 07:06:21 -0400

    ci: add cmake --parallel build option
    Drop nproc call and inconsistent -j setting.

--- ci/scripts/ci.sh
+++ ci/scripts/ci.sh
@@ -24,15 +24,14 @@ ver_ge() { [ "$(printf '%s\n' "$2" "$1" | sort -V | head -n1)" = "$2" ]; }
 src_dir=$PWD
 mkdir -p "$CI_DIR"
 cd "$CI_DIR"
-export CMAKE_BUILD_PARALLEL_LEVEL="$(nproc)"
 cmake "$src_dir" "${CMAKE_ARGS[@]+"${CMAKE_ARGS[@]}"}"
 if ver_ge "$cmake_ver" "3.15"; then
-  cmake --build . -t "${BUILD_TARGETS[@]}" -- "${BUILD_ARGS[@]+"${BUILD_ARGS[@]}"}"
+  cmake --build . --parallel -t "${BUILD_TARGETS[@]}" -- "${BUILD_ARGS[@]+"${BUILD_ARGS[@]}"}"
 else
   # Older versions of cmake can only build one target at a time with --target,
   # and do not support -t shortcut
   for t in "${BUILD_TARGETS[@]}"; do
-    cmake --build . --target "$t" -- "${BUILD_ARGS[@]+"${BUILD_ARGS[@]}"}"
+    cmake --build . --parallel --target "$t" -- "${BUILD_ARGS[@]+"${BUILD_ARGS[@]}"}"
   done
 fi
 ctest --output-on-failure
