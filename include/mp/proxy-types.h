11c023e1006dae68f237a77d1b8005f47c8b9196
Ryan Ofsky <ryan@ofsky.org>
2026-01-13 11:33:50 -0500

    Prevent crash on unclean disconnect if abandoned IPC call returns interface pointer
    This is a fix for https://github.com/bitcoin/bitcoin/issues/34250 which reports
    that bitcoin node crashes if a rust stratrumv2 mining client calls
    BlockTemplate.waitNext() and disconnects without waiting for a response from
    the call, and if mempool fees increased so the call returns a non-null
    interface BlockTemplate pointer.
    
    The node would crash in this case while trying to call MakeProxyServer on the
    returned BlockTemplate pointer, which would fail because MakeProxyServer would
    try to use a reference to the Connection object that had been deleted as a as a
    result of the disconnect.
    
    The fix works by:
    
    - Adding a Connection::m_canceler member variable and using it to cancel any
      IPC response promises that are pending when the connection is destroyed.
    
    - Updating type-context.h PassField() function to use promise.attach() as
      described https://capnproto.org/cxxrpc.html#cancellation to detect
      cancellation and set a ServerContext::m_canceled variable.
    
    - Updating ServerCall to check the ServerContext::m_canceled status after any
      C++ server method returns, and throw an exception if it is set.
    
    - Updating type-context.h PassField() function to deal with the exception
      by catching and logging it, and to deal with canceled status by not
      trying to fulfill the canceled promise.

--- include/mp/proxy-types.h
+++ include/mp/proxy-types.h
@@ -445,6 +445,23 @@ struct ServerCall
     template <typename ServerContext, typename... Args>
     decltype(auto) invoke(ServerContext& server_context, TypeList<>, Args&&... args) const
     {
+        // If cancel_lock is set, release it while executing the method, and
+        // reacquire it afterwards. The lock is needed to prevent params and
+        // response structs from being deleted by the event loop thread if the
+        // request is canceled, so it is only needed before and after method
+        // execution. It is important to release the lock during execution
+        // because the method can take arbitrarily long to return and the event
+        // loop will need the lock itself in on_cancel if the call is canceled.
+        if (server_context.cancel_lock) server_context.cancel_lock->m_lock.unlock();
+        KJ_DEFER(
+            if (server_context.cancel_lock) server_context.cancel_lock->m_lock.lock();
+            // If the IPC request was canceled, there is no point continuing to
+            // execute. It's also important to stop executing because the
+            // connection may have been destroyed as described in
+            // https://github.com/bitcoin/bitcoin/issues/34250 and there would
+            // be a crash if execution continued.
+            if (server_context.request_canceled) throw InterruptException{"canceled"};
+        );
         return ProxyServerMethodTraits<typename decltype(server_context.call_context.getParams())::Reads>::invoke(
             server_context,
             std::forward<Args>(args)...);
