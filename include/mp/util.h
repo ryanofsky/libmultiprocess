a9633f981408fd2190572567e26f2528dcd5ce99
Ryan Ofsky <ryan@ofsky.org>
2026-07-17 07:28:07 -0400

    Improve SpawnProcess API and documentation

--- include/mp/util.h
+++ include/mp/util.h
@@ -277,22 +277,19 @@ using ProcessId = int;
 using SocketId = int;
 constexpr SocketId SocketError{-1};
 
-//! Information about parent process passed to child process as a command-line
-//! argument. On unix this is the child socket fd number formatted as a string.
-using SpawnConnectInfo = std::string;
-
-//! Callback type used by SpawnProcess below.
-using SpawnConnectInfoToArgsFn = std::function<std::vector<std::string>(const SpawnConnectInfo&)>;
-
 //! Spawn a new process that communicates with the current process over a socket
-//! pair. Calls connect_info_to_args callback with a connection string that
-//! needs to be passed to the child process, and executes the argv command line
-//! it returns. Returns child process id and socket id.
-std::tuple<ProcessId, SocketId> SpawnProcess(SpawnConnectInfoToArgsFn&& connect_info_to_args);
+//! pair. Calls spawn_argv callback with a connection string that needs to be
+//! passed to the child process, and executes the argv command line it returns.
+//! Returns child process id and socket id.
+//!
+//! The connection string is just a file descriptor number on unix, and the
+//! child process can call SpawnProcess to parse it.
+std::tuple<ProcessId, SocketId> SpawnProcess(const std::function<std::vector<std::string>(std::string)>& spawn_argv);
 
-//! Initialize spawned child process using the SpawnConnectInfo string passed to it,
-//! returning a socket id for communicating with the parent process.
-SocketId StartSpawned(const SpawnConnectInfo& connect_info);
+//! Initialize spawned child process using the connection string passed by
+//! SpawnProcess through its command line arguments. Returns socket id for
+//! communicating with the parent process.
+SocketId StartSpawned(const std::string& connect_info);
 
 //! Create a socket pair that can be used to communicate within a process or
 //! between parent and child processes.
